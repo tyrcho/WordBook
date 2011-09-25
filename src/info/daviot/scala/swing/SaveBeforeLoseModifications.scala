6e4f03830a44095e2e80e9e42638df9907174003
Michel Daviot <tyrcho@tyrcho.com>
2011-09-25 21:12:52 +0200

    trait to check for modifications and save before exit application

---
+++ src/info/daviot/scala/swing/SaveBeforeLoseModifications.scala
@@ -0,0 +1,31 @@
+package info.daviot.scala.swing
+import java.io.File
+import scala.swing._
+import scala.swing.Dialog._
+
+trait SaveBeforeLoseModifications {
+  var modified: Boolean = false
+  var title = "Confirm"
+  var message = "Save before exit ?"
+  var optionPaneParent: Component = null
+
+  /**
+   * Tries to save, return true for success, false for failure.
+   * Exception will no be handled by the trait.
+   */
+  def save: Boolean
+
+  def checkAndSave(): Boolean = {
+    if (modified) {
+      Dialog.showConfirmation(optionPaneParent, message, title, Options.YesNoCancel) match {
+        case Result.Yes =>
+          if (save) {
+            modified = false
+            true
+          } else false
+        case Result.No => true
+        case _ => false
+      }
+    } else true
+  }
+}
