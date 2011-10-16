a005fd9c56a50d5b1a5e8a605f1880bb283ac22b
Michel Daviot <tyrcho@tyrcho.com>
2011-10-16 22:21:41 +0200

    WordsListDisplay

---
+++ src/info/daviot/dictionary/view/WordsDisplayAsList.scala
@@ -0,0 +1,6 @@
+package info.daviot.dictionary.view;
+
+import javax.swing.DefaultListModel;
+import javax.swing.JList;
+
+class WordsDisplayAsList(listModel: DefaultListModel) extends JList(listModel) with WordsListDisplay
