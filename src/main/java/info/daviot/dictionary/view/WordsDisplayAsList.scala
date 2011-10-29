60245ff0afe02cf87d17ec8c413b2af53ec57ddd
Michel Daviot <tyrcho@tyrcho.com>
2011-10-29 21:16:14 +0200

    fixed project so it still works in Eclipse (build path, resources)

--- src/main/java/info/daviot/dictionary/view/WordsDisplayAsList.scala
+++ src/main/java/info/daviot/dictionary/view/WordsDisplayAsList.scala
@@ -3,4 +3,4 @@ package info.daviot.dictionary.view
 import javax.swing.DefaultListModel
 import javax.swing.JList
 
-class WordsDisplayAsList(listModel: DefaultListModel) extends JList(listModel) with WordsListDisplay
+class WordsDisplayAsList(listModel: DefaultListModel) extends JList(listModel)
